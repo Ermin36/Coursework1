14282f2d044f7599f188b339aec08d359304764d
Рикки <erikgerashchenko@mail.ru>
2026-03-30 10:02:14 +0300

    Обновлены данные модуля reports.py

--- src/reports.py
+++ src/reports.py
@@ -1,18 +1,36 @@
-from calendar import month
-
+from functools import wraps
 import pandas as pd
 from typing import Optional
 import datetime as dt
+from typing import Callable, Any
 
-from src import Operations, Operation
-
-
-def decor_reports() -> callable:
-    pass
+from src import Operations
 
 
-def decor_reports_arg() -> callable:
-    pass
+def decor_reports(func: Callable) -> Callable:
+    """Декоратор записи данных отчётов в файл test.rep"""
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        result: pd.DataFrame = func(*args, **kwargs)
+        result.to_csv('../reports/test.rep', sep='|', index=False)
+        return result
+    return wrapper
+
+
+def decor_reports_arg(file_name: str) -> Callable:
+    """
+    Декоратор записи данных отчётов в файл
+    :param file_name: имя файла
+    :return: декоратор
+    """
+    def wrapper(func: Callable) -> Callable:
+        @wraps(func)
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            result = func(*args, **kwargs)
+            result.to_csv(f'../reports/{file_name}.rep', sep='|', index=False)
+            return result
+        return inner
+    return wrapper
 
 
 def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
@@ -20,14 +38,14 @@ def spending_by_category(transactions: pd.DataFrame, category: str, date: Option
     Функция сбора трат по категории за заданную дату времени
     :param transactions: список транзакций
     :param category: категория по которой происходит сортировка
-    :param date: дата в формате YYYY.MM
+    :param date: дата в формате YYYY.MM.DD
     :return: траты по категории
     """
 
     if date is None:
         end_date = pd.Timestamp.now()
     else:
-        datetime_date =  dt.datetime.strptime(date,'%Y.%m')
+        datetime_date =  dt.datetime.strptime(date,'%Y.%m.%d')
         end_date = pd.Timestamp(datetime_date)
 
     start_date = end_date - pd.DateOffset(months=3) # получение даты за 3 месяца до текущей
