846d8e1364481c26673f1fdcb91c9e16e4bdceb0
Рикки <erikgerashchenko@mail.ru>
2026-03-30 10:09:45 +0300

    Обновлён src.init.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,14 @@
 from .utils import read_json
 from .classes import Operations, Operation
+from .views import get_json_out
+from .services import investment_bank
+from .reports import spending_by_category, decor_reports, decor_reports_arg
 
 
 __all__ = [
     "read_json",
-    "Operations", "Operation"
+    "Operations", "Operation",
+    'get_json_out',
+    'investment_bank',
+    'spending_by_category', 'decor_reports', 'decor_reports_arg'
 ]
